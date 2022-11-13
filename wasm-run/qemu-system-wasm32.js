e4eae480773de39a31ac4ec2d007585009b372c0
Cheng Shao <astrohavoc@gmail.com>
2022-11-13 18:19:14 +0000

    Add back missing functionalities from legacy branch

---
+++ wasm-run/qemu-system-wasm32.js
@@ -0,0 +1,26 @@
+#!/usr/bin/env -S deno run --allow-read --allow-write --v8-flags=--no-liftoff,--wasm-lazy-compilation,--wasm-lazy-validation
+
+import WasiContext from "https://deno.land/std@0.163.0/wasi/snapshot_preview1.ts";
+import * as path from "https://deno.land/std@0.163.0/path/mod.ts";
+
+function parseArgv(args) {
+  const i = args.indexOf("-0");
+  return args.slice(i + 2);
+}
+
+const argv = parseArgv(Deno.args);
+const argv0 = await Deno.realPath(argv[0]);
+
+const context = new WasiContext({
+  args: argv,
+  env: { PATH: "" },
+  preopens: { "/": path.parse(argv0).dir },
+});
+
+const instance = (
+  await WebAssembly.instantiate(await Deno.readFile(argv0), {
+    wasi_snapshot_preview1: context.exports,
+  })
+).instance;
+
+context.start(instance);
