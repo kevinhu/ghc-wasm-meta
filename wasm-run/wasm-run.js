426f47cec08741f0c4c463c4620cd9c72200e591
Cheng Shao <astrohavoc@gmail.com>
2022-12-10 21:04:33 +0000

    Make wasm-run.js a standalone runtime

--- wasm-run/wasm-run.js
+++ wasm-run/wasm-run.js
@@ -1,14 +1,13 @@
-#!/usr/bin/env -S deno run --allow-read --allow-write --v8-flags=--no-liftoff,--wasm-lazy-compilation,--wasm-lazy-validation
+#!/usr/bin/env -S deno run --allow-read --allow-write --v8-flags=--experimental-wasm-return_call,--no-liftoff,--wasm-lazy-compilation,--wasm-lazy-validation
 
 import WasiContext from "https://deno.land/std@0.167.0/wasi/snapshot_preview1.ts";
 
 function parseArgv(args) {
   const i = args.indexOf("-0");
-  return args.slice(i + 2);
+  return i === -1 ? args : args.slice(i + 2);
 }
 
 const argv = parseArgv(Deno.args);
-const argv0 = await Deno.realPath(argv[0]);
 
 const context = new WasiContext({
   args: argv,
@@ -17,7 +16,7 @@ const context = new WasiContext({
 });
 
 const instance = (
-  await WebAssembly.instantiate(await Deno.readFile(argv0), {
+  await WebAssembly.instantiate(await Deno.readFile(argv[0]), {
     wasi_snapshot_preview1: context.exports,
   })
 ).instance;
